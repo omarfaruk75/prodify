b4ff67915d6585c871a381f8e1bbdb7fb0b3b725
omar faruk <farukomarcpa@gmail.com>
2024-08-17 17:49:24 +0600

    image size updated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,14 +4,14 @@ const ProductCard = ({ product }) => {
 
     return (
         <div className="my-2">
-            <div className="flex flex-col bg-base-100  shadow-2xl py-6 h[304px]">
-                <figure className="px-10  flex-1">
+            <div className="flex flex-col bg-base-100  shadow-2xl py-6 h-[400px]">
+                <figure className="px-10  flex-1 ">
                     <img
                         src={image}
                         alt="Shoes"
-                        className="rounded-xl" />
+                        className="rounded-xl h-[200px] w-full" />
                 </figure>
-                <div className="px-10 pt-6 flex-1">
+                <div className="px-10 py-4 flex-1">
                     <h2 className="card-title"> {productName}</h2>
                     <p className="">Description: {description?.substring(0, 40)}</p>
                     <p className="flex"><span className="flex-1">Price: {price} </span> <span>Category: {category}</span></p>
